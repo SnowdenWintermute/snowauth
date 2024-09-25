163ade3ccebd2ae3e2ffca0b22109c9f6f62cf4a
Mike <michael.p.silverman@gmail.com>
2024-09-25 13:46:48 -0400

    organizing thoughts about routes

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,34 @@ console.log("pg connected");
 
 const app = express();
 
+app.get("/", (req, res) => res.send("You have reached the root route of the snowauth server"));
+// USEABLE BY ANYONE
+//
+// - registration
+// app.post("/users", registrationIpRateLimiter, validate(registerUserSchema), registerNewAccountHandler);
+// - account activation
+// router.put("/users", accountActivationHandler);
+// - login
+// router.post("/sessions", validate(loginSchema), loginHandler);
+// - get change password email
+// router.post("/credentials", passwordResetEmailRequestIpRateLimiter, passwordResetEmailRequestHandler);
+// - change password using token in email
+// router.put("/credentials/:password_change_token", validate(changePasswordSchema), changePasswordHandler);
+
+// LOGGED IN USERS ONLY ONLY
+// router.use(deserializeUser, refreshSession);
+// - get profile
+// router.get("/users", getProfileHandler);
+// - delete session
+// router.delete("/sessions", deserializeUser, logoutHandler);
+// - delete user account
+// router.delete("/users/:user_id", deleteAccountHandler);
+
+// MODERATOR/ADMIN ONLY
+// router.use(restrictTo(UserRole.MODERATOR, UserRole.ADMIN));
+// - ban account
+// router.put(/users/bans/:user_id, banUserAccountHandler);
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
